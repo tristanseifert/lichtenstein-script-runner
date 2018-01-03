404bde2c8b86de244e23f3d25d2038005c25d47c
Tristan Seifert <t@tseifert.me>
2018-01-03 15:45:33 -0600

    Initial Commit

---
+++ Lichtenstein Script Tester/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lichtenstein Script Tester
+//
+//  Created by Tristan Seifert on 2018-01-03.
+//  Copyright © 2018 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Lichtenstein Script Tester/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  Lichtenstein Script Tester
+//
+//  Created by Tristan Seifert on 2018-01-03.
+//  Copyright © 2018 Tristan Seifert. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
